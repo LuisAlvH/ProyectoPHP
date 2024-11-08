f5654580b744fa73e4e01f792ab9dc999b5c6672
Alvaro <luishinostroza_1@hotmail.com>
2024-11-08 14:34:52 -0300

    Version Laravel Veterinaria

---
+++ Laravel/veterinaria/app/Http/Middleware/Admin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+//use Illuminate\Container\Attributes\Auth;
+
+use Illuminate\Support\Facades\Auth;
+
+
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(Auth::user()->usertype !='admin'){
+
+            return redirect('dashboard');
+        }else if(Auth::user()->usertype !='vendedor'){
+
+            return redirect('dashboard');
+        }
+        return $next($request);
+    }
+
+
+    
+}
